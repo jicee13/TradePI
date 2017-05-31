a57669fefee1b2c67344e9867073c56214539433
Jayce Miller <jaycem@smu.edu>
2017-05-31 14:08:28 -0500

    cleaned out stupid data and got some test data

--- code/practicePage.py
+++ code/practicePage.py
@@ -7,11 +7,21 @@ import time
 import csv
 import json
 
-response = urllib.request.urlopen('https://finance.yahoo.com/quote/AAPL/key-statistics')
+response = urllib.request.urlopen('https://finance.yahoo.com/quote/afi/key-statistics')
 soup = bs4.BeautifulSoup(response,"lxml")
 #Remove junk data
 [s.extract() for s in soup('script')]
 [s.extract() for s in soup('meta')]
+[s.extract() for s in soup('head')]
+soup.find('div', id='masterNav').extract()
+soup.find('div', id='YDC-UH').extract()
+soup.find('div', id='Navigation').extract()
+soup.find('div', id='YDC-Lead').extract()
+soup.find('div', id='YDC-Col2').extract()
+
+
+
+#print(soup)
 try:
     pegRatioSpan = soup.findAll('span', string='PEG Ratio (5 yr expected)')[0].parent.parent
 except IndexError:
@@ -20,5 +30,5 @@ else:
     pegText = pegRatioSpan.findAll('span')[0].text
     pegValue = pegRatioSpan.findAll('td')[1].text
 
-print(pegText)
-print(pegValue)
+# print(pegText)
+# print(pegValue)
